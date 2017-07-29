e9313924c79eec139cb696f89e0b54258ab2cd7d
markitus18 <marc18.garrigo@gmail.com>
2017-07-29 13:20:14 +0200

    Randomly generating "bodies"

---
+++ Ludum Dare/Assets/Scripts/AspectDatabase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class AspectDatabase : MonoBehaviour {
+
+    public string path;
+
+    public Sprite[] heads;
+    public Sprite[] bodies;
+    public Sprite[] faces;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        heads = Resources.LoadAll<Sprite>(path + "/Head");
+        bodies = Resources.LoadAll<Sprite>(path + "/Body");
+        faces = Resources.LoadAll<Sprite>(path + "/Face");
+    }
+
+    public Sprite GetHead()
+    {
+        int index = Random.Range(0, heads.Length);
+        return heads[index];
+    }
+
+    public  Sprite GetBody()
+    {
+        int index = Random.Range(0, bodies.Length);
+        return bodies[index];
+    }
+
+    public Sprite GetFace()
+    {
+        int index = Random.Range(0, faces.Length);
+        return faces[index];
+    }
+}
---
+++ Ludum Dare/Assets/Scripts/GenerateAspect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateAspect : MonoBehaviour {
+
+    public SpriteRenderer head;
+    public SpriteRenderer body;
+    public SpriteRenderer face;
+
+    public AspectDatabase database;
+
+    // Use this for initialization
+    void Start ()
+    {
+        head.sprite = database.GetHead();
+        body.sprite = database.GetBody();
+        face.sprite = database.GetFace();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
