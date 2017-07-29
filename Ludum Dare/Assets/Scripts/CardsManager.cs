249a0ac1344eb4223f390a11bd0c28d3a49ceefa
markitus18 <marc18.garrigo@gmail.com>
2017-07-30 01:46:40 +0200

    Starting card manager

---
+++ Ludum Dare/Assets/Scripts/CardsManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardsManager : MonoBehaviour
+{
+    public TextManager textManager;
+    public GameObject cardPanel;
+    public Text cardText;
+    public string cardsPath;
+
+    public TextAsset[] cards;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cards = Resources.LoadAll<TextAsset>(cardsPath);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void OnClickCard()
+    {
+        cardText.text = cards[textManager.day - 1].text;
+        cardPanel.SetActive(true);
+    }
+
+    public void OnExitButton()
+    {
+        cardPanel.SetActive(false);
+    }
+}
