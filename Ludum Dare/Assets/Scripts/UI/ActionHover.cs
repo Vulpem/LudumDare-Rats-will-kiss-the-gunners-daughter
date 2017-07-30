698dc569f22b2fdc0eacf8e51604c444d6845d44
markitus18 <marc18.garrigo@gmail.com>
2017-07-30 16:40:11 +0200

    New UI feedback scripts

---
+++ Ludum Dare/Assets/Scripts/UI/ActionHover.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionHover : MonoBehaviour {
+
+    bool moving = false;
+    bool movingOut = false;
+    Vector2 originalSize;
+
+    public Vector2 hoverSize;
+    public float movementTime = 0.5f;
+    float time = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        originalSize = gameObject.GetComponent<RectTransform>().sizeDelta;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (moving == true)
+        {
+            time += Time.deltaTime;
+            Vector2 newSize;
+            if (movingOut == true)
+            {
+                newSize = Vector2.Lerp(originalSize, hoverSize, time / movementTime);
+            }
+            else
+            {
+                newSize = Vector3.Lerp(hoverSize, originalSize, time / movementTime);
+            }
+            if (time >= movementTime)
+            {
+                moving = false;
+                newSize = movingOut ? hoverSize : originalSize;
+            }
+            gameObject.GetComponent<RectTransform>().sizeDelta = newSize;
+        }
+	}
+
+    public void OnHover()
+    {
+        moving = true;
+        movingOut = true;
+        time = 0.0f;
+    }
+
+    public void OnExit()
+    {
+        moving = true;
+        movingOut = false;
+        time = 0.0f;
+    }
+}
---
+++ Ludum Dare/Assets/Scripts/UI/CardMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardMovement : MonoBehaviour {
+
+    bool moving = false;
+    bool movingOut = false;
+    Vector3 originalPosition;
+
+    public Vector3 movedPosition;
+    public float movementTime = 0.5f;
+    float time = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        originalPosition = gameObject.GetComponent<RectTransform>().anchoredPosition;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (moving == true)
+        {
+            time += Time.deltaTime;
+            Vector3 newPosition;
+            if (movingOut == true)
+            {
+                newPosition = Vector3.Lerp(originalPosition, movedPosition, time / movementTime);
+            }
+            else
+            {
+                newPosition = Vector3.Lerp(movedPosition, originalPosition, time / movementTime);
+            }
+            if (time >= movementTime)
+            {
+                moving = false;
+                newPosition = movingOut ? movedPosition : originalPosition;
+            }
+            gameObject.GetComponent<RectTransform>().anchoredPosition = newPosition;
+        }
+	}
+
+    public void MoveCardUp()
+    {
+        moving = true;
+        movingOut = true;
+        time = 0.0f;
+    }
+
+    public void MoveCardDown()
+    {
+        moving = true;
+        movingOut = false;
+        time = 0.0f;
+    }
+}
